e8a8033cebc4f77b043ad00c7f8deb41394a78d3
Christophe VG <xtof@pumpkin.local>
2009-07-01 00:28:07 +0200

    added user agent sniffing

--- src/ProtoJS.js
+++ src/ProtoJS.js
@@ -1,1 +1,11 @@
-var ProtoJS = {};
+var ProtoJS = {
+  Browser: {
+    IE:     !!(window.attachEvent &&
+      navigator.userAgent.indexOf('Opera') === -1),
+    Opera:  navigator.userAgent.indexOf('Opera') > -1,
+    WebKit: navigator.userAgent.indexOf('AppleWebKit/') > -1,
+    Gecko:  navigator.userAgent.indexOf('Gecko') > -1 &&
+      navigator.userAgent.indexOf('KHTML') === -1,
+    MobileSafari: !!navigator.userAgent.match(/Apple.*Mobile.*Safari/)
+  }
+};
