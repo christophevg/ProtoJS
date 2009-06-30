b3d85f4f65af930ebf465749ea3968658d10d461
Christophe VG <xtof@pumpkin.local>
2009-07-01 00:17:31 +0200

    added contains method to String

--- src/String.js
+++ src/String.js
@@ -1,4 +1,8 @@
 ProtoJS.String = {
+    contains : function( needle ) {
+	return this.indexOf( needle ) > 0;
+    },
+
     isArray   : function() { return false; },
     isHash    : function() { return false; },
     isFunction: function() { return false; },
