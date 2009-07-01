01b28dd32fbf4e4460e46fa87906223c8032213c
Christophe VG <xtof@pumpkin.local>
2009-07-01 21:59:05 +0200

    added support for compare, dup, remove, clear to Array

--- src/Array.js
+++ src/Array.js
@@ -1,4 +1,15 @@
 ProtoJS.Array =  {
+    compare: function(other) {
+	if (this.length != other.length) return false;
+	for (var i = 0; i < other.length; i++) {
+            if (this[i].compare) { 
+		if (!this[i].compare(other[i])) return false;
+            }
+            if (this[i] !== other[i]) return false;
+	}
+	return true;
+    },
+
     has: function(needle) {
 	return this.indexOf(needle) > -1;
     },
@@ -7,6 +18,25 @@ ProtoJS.Array =  {
 	for(var i=0, length=this.length; i<length; i++ ) {
 	    handler.call(context, this[i], i);
 	}
+    },
+
+    dup: function() {
+	return [].concat(this);
+    },
+
+    clear: function() {
+	this.length = 0;
+	return this;
+    },
+
+    remove: function() {
+	var needles = $A(arguments);
+	var old = this.dup();
+	this.clear();
+	old.iterate( function(item) { 
+	    if( !needles.has(item) ) { this.push(item); }
+	}.scope(this) );
+	return this;
     },
 
     isArray   : function() { return true;  },
