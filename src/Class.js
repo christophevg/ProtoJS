fbbfa494aaa0897ffc8fe6b991ba71b2777338b8
Christophe VG <xtof@pumpkin.local>
2009-10-05 12:06:24 +0200

    fixes bug#263

--- src/Class.js
+++ src/Class.js
@@ -24,29 +24,47 @@
 	prototype.isNumber   = function() { return false; };
 	prototype.isString   = function() { return false; };
 	
-	// Copy the properties over onto the new prototype
-	for (var name in prop) {
-	    // Check if we're overwriting an existing function
-	    prototype[name] = typeof prop[name] == "function" &&
-		typeof _super[name] == "function" && fnTest.test(prop[name]) ?
-		(function(name, fn){
-		    return function() {
-			var tmp = this._super;
-			
-			// Add a new ._super() method that is the same method
-			// but on the super-class
-			this._super = _super[name];
-			
-			// The method only need to be bound temporarily, so we
-			// remove it when we're done executing
-			var ret = fn.apply(this, arguments);       
-			this._super = tmp;
-			
-			return ret;
-		    };
-		})(name, prop[name]) :
-            prop[name];
-	}
+  // A function to create a wrapped inherited method
+  function _make_wrapped_method(name, fn) {
+    return function() {
+      var tmp = this._super;
+
+      // Add a new ._super() method that is the same method
+      // but on the super-class
+      this._super = _super[name];
+
+      // The method only need to be bound temporarily, so we
+      // remove it when we're done executing
+      var ret = fn.apply(this, arguments);
+      this._super = tmp;
+
+      return ret;
+    };
+  }
+
+  // Copy the properties over onto the new prototype
+  for (var name in prop) {
+    // Check if we're overwriting an existing function
+    prototype[name] = typeof prop[name] == "function" &&
+    typeof _super[name] == "function" && fnTest.test(prop[name]) ?
+    _make_wrapped_method( name, prop[name] ) : prop[name];
+  }
+
+  // toString doesn't show up in the iterated properties in JScript
+  // see: https://developer.mozilla.org/en/ECMAScript_DontEnum_attribute
+  // quick fix: explicitly test for it
+  if( ProtoJS.Browser.IE && prop["toString"] ) {
+    prototype["toString"] =
+      _make_wrapped_method( "toString", prop["toString"]);
+  }
+
+  // The dummy class constructor
+  function Class() {
+    // All construction is actually done in the init method
+    if ( !initializing && this.init )
+    this.init.apply(this, arguments);
+  }
+
 	
 	// The dummy class constructor
 	function Class() {
