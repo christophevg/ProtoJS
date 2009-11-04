70d4c16806cfc593223bcffd7fb3bc3be0e2ecb1
Christophe VG <xtof@pumpkin.local>
2009-11-04 22:18:26 +0100

    added trim support to String class cleaned up indentation of String class

--- src/String.js
+++ src/String.js
@@ -1,13 +1,18 @@
 ProtoJS.String = {
-    contains : function( needle ) {
-	return this.indexOf( needle ) > -1;
-    },
+  contains : function( needle ) {
+    return this.indexOf( needle ) > -1;
+  },
 
-    isArray   : function() { return false; },
-    isHash    : function() { return false; },
-    isFunction: function() { return false; },
-    isString  : function() { return true;  },
-    isNumber  : function() { return false; }
+  trim : function( value ) {
+    if( typeof this.replace == "function" ) { value = this; }
+    return value.replace(/^\s*/, "").replace(/\s*$/, "");
+  },
+
+  isArray   : function() { return false; },
+  isHash    : function() { return false; },
+  isFunction: function() { return false; },
+  isString  : function() { return true;  },
+  isNumber  : function() { return false; }
 };
 
 ProtoJS.mix( ProtoJS.String, String.prototype );
