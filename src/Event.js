37e8745fc6ad63bd5af942f1b229d013793d693c
Christophe VG <xtof@pumpkin.local>
2009-11-23 15:27:33 +0100

    added input validation and warning

--- src/Event.js
+++ src/Event.js
@@ -1,12 +1,15 @@
 // this is a very minimalistic first stab
 ProtoJS.Event = {
-    observe: function(element, eventName, handler) {
-	if (element.addEventListener) {
-            element.addEventListener(eventName, handler, false);
-	} else {
-            element.attachEvent("on" + eventName, handler);
-	}
-	
-	return element;
+  observe: function(element, eventName, handler) {
+    if(!element) { 
+      console.log( "WARN: passed invalid element to ProtoJS.Event.observe." );
+    } else {
+      if (element.addEventListener) {
+        element.addEventListener(eventName, handler, false);
+      } else {
+        element.attachEvent("on" + eventName, handler);
+      }
     }
+    return element;
+  }
 }
