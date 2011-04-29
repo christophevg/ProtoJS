21641c2c2af675e83a52879289540bc20f2d68cf
Christophe VG <xtof@astroboy.local>
2011-04-29 23:48:37 +0200

    radically changed the include.js implementation include.js now finds its own location and includes scripts relative to this path. This make it a lot easier to reuse the include.js files in nested situations.

--- include.js
+++ include.js
@@ -1,4 +1,32 @@
-var scripts = [ 
+(function(project, scripts) {
+	var cwd = (function cwd() {
+		var scripts = document.getElementsByTagName('script');
+		var re = new RegExp( project + "\/include\.js$");
+		for( var i in scripts) {
+			if( scripts[i].src && scripts[i].src.match( re ) ) {
+				return scripts[i].src.replace(/(.*)include\.js$/, '$1');
+			}
+		}
+	})();
+
+	var include = function include(url) {
+		document.writeln( "<script type=\"text/javascript\" " +
+											"        src=\"" + url + "\"></scr" + "ipt>" );
+	}
+
+	for( var i=0; i<scripts.length; i++ ) {
+		include( cwd + scripts[i]);
+	}
+})
+(
+
+// The name of the project is needed to select the correct include.js file.
+// When reusing projects with nested include files this might be required.
+"ProtoJS",
+
+// The following list are the separate Javascript files, in order, to be
+// loaded. They are relative to this include file.
+[
   "src/ProtoJS.js", 
   "src/Mixin.js", 
   "src/Object.js", 
@@ -9,20 +37,6 @@ var scripts = [
   "src/Hash.js",    
   "src/Function.js",
   "src/Event.js"
-];
+]
 
-function addScript(url) {
-  document.writeln( "<script type=\"text/javascript\" src=\"" + url + "\"></script>" );
-}
-
-function loadScripts(prefix) {
-  for( var i=0; i<scripts.length; i++ ) {
-    addScript(prefix + scripts[i]);
-  }
-}
-
-if( typeof window.loadingPrefix == "undefined" ) {
-  window.loadingPrefix = "../";
-}
-
-loadScripts(window.loadingPrefix);
+);
